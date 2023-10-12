f828e00c9aa2a212d43459c393bc9b6b6b64c331
hareesh <hareesh.vanapalli.888@gmail.com>
2023-10-12 15:39:36 +0530

    [hari]

---
+++ Day11_CollectionPracticeProblem/Max_Selling_Price.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11_CollectionPracticeProblem
+{
+    internal class Max_Selling_Price
+    {
+    }
+}
---
+++ Day11_CollectionPracticeProblem/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Day11_CollectionPracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose any program");
+            Console.WriteLine("1. Count of remaining chepters\n" +
+                "2. Maximaum selling price");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    CountOfRemainingChepters.RemainingChepters();
+                    break;
+                case 2:
+                    MaximumSellingPrice.MaximumSellingPriceBySwapping();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid use case number");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
